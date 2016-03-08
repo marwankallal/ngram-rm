4d7477aeaa1543e187489882a3644ad32e4f9a16
Marwan Kallal <marwan.kallal@gmail.com>
2016-03-08 15:26:04 -0500

    branch extension and creating new branch now one step

--- src_c/ngram-rm.c
+++ src_c/ngram-rm.c
@@ -10,17 +10,19 @@ int main(){
 /* This step will make the subsequent deletion significantly faster
    especially when using the same set of ngrams on many strings 
    NOTE: The ngrams must be null-terminated */
-void preprocess(node *root, char **ngrams){
+void preprocess(node *root, char **ngrams, unsigned int ngram_cnt){
+    
     node *currnode = root;
     char *ngram = ngrams[0];
     char *currchar = ngram;
 
     while(1){
 
-        // Continue the branch
-        if(currnode->child_cnt == 0){
+        // Extend the current branch or start a new one
+        if(currnode->child_cnt == 0 || !has_child(currnode, *currchar)){
             node *newchild = (node *)malloc(sizeof(node));
             newchild->depth = currnode->depth + 1;
+            newchild->in_edge = *currchar;
 
             char *childval = (char *)malloc(newchild->depth);
             memcpy(childval, currnode->value, currnode->depth);
@@ -30,11 +32,22 @@ void preprocess(node *root, char **ngrams){
             currnode->child_cnt++;
             currnode->children = (node **)malloc(sizeof(node *));
             currnode->children[0] = newchild;
-        } else {
-            //TODO: create new branch
+        } 
+        
+        //TODO: Follow the current branch
+        else {
+             
         }
 
+        // Go to the next character of the current ngram
         currchar += sizeof(char);
+
+        // Check we haven't run out of ngrams
+        if(ngram_cnt == 0){
+            break;
+        }
+        ngram_cnt--;
+
         // Move to next ngram
         if(*currchar == 0){
             ngram = *(&ngram + sizeof(char *));
@@ -44,6 +57,19 @@ void preprocess(node *root, char **ngrams){
 
 
     return;
+}
+
+short has_child(node *currnode, char childchar){
+    int cnt;
+    node *child = currnode->children[0];
+    for(cnt = 0; cnt < currnode->child_cnt; cnt++){
+        if (child->in_edge == childchar){
+            return 1;
+        }
+        child = *(&child + sizeof(node *));
+    }
+
+    return 0;
 }
 
 char *ngram_rm(node *root, char *str){
--- src_c/ngram-rm.h
+++ src_c/ngram-rm.h
@@ -5,7 +5,9 @@ struct node{
     char *value;
     int child_cnt;
     int depth;
+    char in_edge;
 };
 
-void build_ngrams(node *root, char **ngrams);
+void preprocess(node *root, char **ngrams, unsigned int ngram_cnt);
 char *ngram_rm(node *root, char* str);
+short has_child(node *currnode, char childchar);
